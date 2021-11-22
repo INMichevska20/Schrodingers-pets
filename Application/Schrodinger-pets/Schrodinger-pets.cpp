9ba54f53b474a3eee33e9cdd47a06ddc7a889518
Димитър Василев <DSVasilev20@codingburgas.bg>
2021-11-22 18:08:54 +0200

    Add rotation function

--- Application/Schrodinger-pets/Schrodinger-pets.cpp
+++ Application/Schrodinger-pets/Schrodinger-pets.cpp
@@ -1,20 +1,14 @@
-// Schrodinger-pets.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+using namespace std;
 
-int main()
+int Rotation(int px, int py, int r)
 {
-    std::cout << "Hello World!\n";
+    switch (r % 4)
+    {
+    case 0: return py * 4 + px;         // 0*
+    case 1: return 12 + py - (px * 4);  // 90*
+    case 2: return 15 - (py * 4) - px;  // 180*
+    case 3: return 3 - py + (px * 4);   // 270*
+    }
+    return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
