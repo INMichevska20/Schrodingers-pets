156d8085bb7408015bdcb68ef5aa2310122661af
Димитър Василев <DSVasilev20@codingburgas.bg>
2021-11-22 19:20:32 +0200

    Create assets

--- Application/Schrodinger-pets/Schrodinger-pets.cpp
+++ Application/Schrodinger-pets/Schrodinger-pets.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <cstring>
+
 using namespace std;
 
 int Rotation(int px, int py, int r)
@@ -11,4 +13,44 @@ int Rotation(int px, int py, int r)
     case 3: return 3 - py + (px * 4);   // 270*
     }
     return 0;
+}
+wstring tetromino[7];
+
+int main()
+{
+    // create assets
+    tetromino[0].append(L"..X.");
+    tetromino[0].append(L"..X.");
+    tetromino[0].append(L"..X.");
+    tetromino[0].append(L"..X.");
+
+    tetromino[1].append(L"..X.");
+    tetromino[1].append(L".XX.");
+    tetromino[1].append(L".X..");
+    tetromino[1].append(L"....");
+
+    tetromino[2].append(L".X..");
+    tetromino[2].append(L".XX.");
+    tetromino[2].append(L"..X.");
+    tetromino[2].append(L"....");
+
+    tetromino[3].append(L"....");
+    tetromino[3].append(L".XX.");
+    tetromino[3].append(L".XX.");
+    tetromino[3].append(L"....");
+
+    tetromino[4].append(L"..X.");
+    tetromino[4].append(L".XX.");
+    tetromino[4].append(L"..X.");
+    tetromino[4].append(L"....");
+
+    tetromino[5].append(L"....");
+    tetromino[5].append(L".XX.");
+    tetromino[5].append(L"..X.");
+    tetromino[5].append(L"..X.");
+
+    tetromino[6].append(L"....");
+    tetromino[6].append(L".XX.");
+    tetromino[6].append(L".X..");
+    tetromino[6].append(L".X..");
 }
